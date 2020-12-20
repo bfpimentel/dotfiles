d1e918c5a98bbd4aefee13a6d8b77c1c1944b22f
Bruno Pimentel <bruno@pimentel.dev>
2020-12-20 20:24:33 -0300

    feat: add remaining files

---
+++ .config/screenlayout/dual-monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --primary --mode 1920x1080 --pos 0x420 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --mode 1920x1080 --pos 1920x0 --rotate right --output HDMI-A-2 --off
