5df952e50dfb060e44cf93fba4ba1ba0cf228da4
Bruno Pimentel <bruno@pimentel.dev>
2020-12-22 15:09:41 -0300

    feat: move more scripts to autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,6 +1,16 @@
 #!bin/sh
 
+# ssh agent setup
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa_personal
 ssh-add ~/.ssh/id_rsa_work
 
+# displays setup
+sh $HOME/.config/screenlayout/dual-monitors.sh
+
+# kb layout setup
+setxkbmap -layout us -variant intl
+
+# wm setup
+nitrogen --restore &
+picom &
