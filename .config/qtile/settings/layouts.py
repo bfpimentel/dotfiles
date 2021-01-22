7a7f08d55081ded1d803b4216da287692e76b977
Bruno Pimentel <bruno@pimentel.dev>
2021-01-22 10:33:41 -0300

    fix: refactor floating layout rules

--- .config/qtile/settings/layouts.py
+++ .config/qtile/settings/layouts.py
@@ -1,4 +1,5 @@
 from libqtile import layout
+from libqtile.config import Match
 from settings.theme import theme
 
 layout_conf = {
@@ -15,21 +16,26 @@ layouts = [
 
 floating_layout = layout.Floating(
     float_rules=[
-        {'wmclass': 'confirm'},
-        {'wmclass': 'dialog'},
-        {'wmclass': 'download'},
-        {'wmclass': 'error'},
-        {'wmclass': 'file_progress'},
-        {'wmclass': 'notification'},
-        {'wmclass': 'splash'},
-        {'wmclass': 'toolbar'},
-        {'wmclass': 'confirmreset'},
-        {'wmclass': 'makebranch'},
-        {'wmclass': 'maketag'},
-        {'wmclass': 'ssh-askpass'},
-        {'wname': 'branchdialog'},
-        {'wname': 'pinentry'},
-        {'wname': 'Picture-in-Picture'},
+        Match(wm_type="utility"),
+        Match(wm_type="notification"),
+        Match(wm_type="toolbar"),
+        Match(wm_type="splash"),
+        Match(wm_type="dialog"),
+        Match(wm_class="confirm"),
+        Match(wm_class="dialog"),
+        Match(wm_class="download"),
+        Match(wm_class="error"),
+        Match(wm_class="file_progress"),
+        Match(wm_class="notification"),
+        Match(wm_class="splash"),
+        Match(wm_class="toolbar"),
+        Match(wm_class="confirmreset"),
+        Match(wm_class="makebranch"),
+        Match(wm_class="maketag"),
+        Match(wm_class="ssh-askpass"),
+        Match(title="branchdialog"),
+        Match(title="pinentry"),
+        Match(title='Picture-in-Picture'),
     ],
     border_focus=theme['focus'][0]
 )
