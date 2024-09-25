5a3d5a5233533ae035f173ee2ace2204b4a23d1c
Bruno Pimentel <hello@bruno.so>
2024-09-25 09:13:20 -0300

    feat: add containers backup

--- nixos/malenia/services/restic/default.nix
+++ nixos/malenia/services/restic/default.nix
@@ -16,5 +16,23 @@
         "--keep-weekly 2"
       ];
     };
+    containers-daily = {
+      initialize = true;
+
+      environmentFile = config.age.secrets.restic-env.path;
+      repositoryFile = config.age.secrets.restic-repo-containers.path;
+      passwordFile = config.age.secrets.restic-password-containers.path;
+
+      paths = [ vars.containersMountLocation ];
+
+      # exclude = [
+      #   "${vars.containersMountLocation}/immich/machine-learning"
+      # ];
+
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 2"
+      ];
+    };
   };
 }
