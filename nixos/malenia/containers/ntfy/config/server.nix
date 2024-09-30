b8fd373383b2457bd6ac00c5730a460214f3a212
Bruno Pimentel <hello@bruno.so>
2024-09-30 12:51:57 -0300

    chore: change ntfy server address

--- nixos/malenia/containers/ntfy/config/server.nix
+++ nixos/malenia/containers/ntfy/config/server.nix
@@ -1,5 +1,5 @@
-externalDomain: {
-  base-url = "https://notify.${externalDomain}";
+domain: {
+  base-url = "https://notify.${domain}";
   listen-http = ":80";
   auth-file = "/etc/ntfy/user.db";
   auth-default-access = "deny-all";
--- nixos/malenia/containers/ntfy/default.nix
+++ nixos/malenia/containers/ntfy/default.nix
@@ -17,7 +17,7 @@ let
   settingsFormat = pkgs.formats.yaml { };
   ntfySettings = {
     server = settingsFormat.generate "${ntfyPath}/data/server.yml" (
-      (import ./config/server.nix) vars.externalDomain
+      (import ./config/server.nix) vars.domain
     );
   };
 in
@@ -38,6 +38,17 @@ in
         "${ntfyPath}/data:/etc/ntfy"
         "${ntfySettings.server}:/etc/ntfy/server.yml"
       ];
+      labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.ntfy.rule" = "Host(`notify.${vars.domain}`)";
+        "traefik.http.routers.ntfy.entryPoints" = "https";
+        "traefik.http.services.ntfy.loadbalancer.server.port" = "80";
+        # Homepage
+        "homepage.group" = "Misc";
+        "homepage.name" = "Ntfy";
+        "homepage.icon" = "ntfy.svg";
+        "homepage.href" = "https://notify.${vars.domain}";
+      };
     };
   };
 }
