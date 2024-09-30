519d0acc05890030de09dca3bc909c47473fb2ed
Bruno Pimentel <hello@bruno.so>
2024-09-30 20:24:17 -0300

    feat: add zsh config to shared

--- home-manager/hosts/shared/linux.nix
+++ home-manager/hosts/shared/linux.nix
@@ -1,6 +1,10 @@
 { username, ... }:
 
 {
+  imports = [
+    ./zsh
+  ];
+
   programs.ssh = {
     enable = true;
     matchBlocks = {
@@ -13,33 +17,5 @@
     extraConfig = ''
       AddKeysToAgent yes
     '';
-  };
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    shellAliases = {
-      cdnix = "cd /etc/nixos";
-      cnix = "nvim /etc/nixos";
-      rnix = "sudo nixos-rebuild switch --impure";
-      vim = "nvim";
-      gst = "git status -sb";
-      gf = "git fetch";
-    };
-    sessionVariables = {
-      EDITOR = "nvim";
-    };
-    initExtra = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-
-      autoload -U colors && colors
-      PS1="%{$fg[green]%}%n%{$reset_color%}@%{$fg[cyan]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
-    '';
-    oh-my-zsh = {
-      enable = true;
-      theme = "robbyrussell";
-      plugins = [ "git" ];
-    };
   };
 }
---
+++ home-manager/hosts/shared/zsh/default.nix
@@ -0,0 +1,14 @@
+{ config, username, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    envExtra = ''
+    ZDOTDIR="/home/${username}/.config/zsh"
+    '';
+  };
+
+  home.file.".config/zsh" = {
+    source = config.lib.file.mkOutOfStoreSymlink ./config;
+  };
+}
