ed304e2a78a6a3f8522801d55ae5c6cbadaabcb1
Bruno Pimentel <hello@bruno.so>
2024-10-02 18:56:49 -0300

    chore: move vaultwarden to local domain

--- nixos/malenia/containers/vaultwarden/default.nix
+++ nixos/malenia/containers/vaultwarden/default.nix
@@ -18,10 +18,9 @@ in
       autoStart = true;
       extraOptions = [ "--pull=newer" ];
       volumes = [ "${vaultwardenPath}:/data" ];
-      ports = [ "9010:80" ];
       environmentFiles = [ config.age.secrets.vaultwarden.path ];
       environment = {
-        DOMAIN = "https://vault.${vars.externalDomain}";
+        DOMAIN = "https://vault.${vars.domain}";
         LOGIN_RATELIMIT_MAX_BURST = "10";
         LOGIN_RATELIMIT_SECONDS = "60";
         ADMIN_RATELIMIT_MAX_BURST = "10";
@@ -31,6 +30,10 @@ in
         WEB_VAULT_ENABLED = "true";
       };
       labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.vaultwarden.rule" = "Host(`vault.${vars.domain}`)";
+        "traefik.http.routers.vaultwarden.entryPoints" = "https";
+        "traefik.http.services.vaultwarden.loadbalancer.server.port" = "80";
         # Homepage
         "homepage.group" = "Management";
         "homepage.name" = "Vaultwarden";
