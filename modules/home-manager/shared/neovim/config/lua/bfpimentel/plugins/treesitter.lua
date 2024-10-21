da1d0f271eb32e77b6fa35716da0d6f13844cbb9
Bruno Pimentel <hello@bruno.so>
2024-10-21 08:25:53 -0300

    chore: add treesitter bindings

--- modules/home-manager/shared/neovim/config/lua/bfpimentel/plugins/treesitter.lua
+++ modules/home-manager/shared/neovim/config/lua/bfpimentel/plugins/treesitter.lua
@@ -2,7 +2,23 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   lazy = false,
+  config = function(_, opts)
+    require('nvim-treesitter.configs').setup(opts)
+  end,
   opts = {
+    sync_install = false,
+    auto_install = true,
+    highlight = { enable = true, additional_vim_regex_highlighting = false },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-n>",
+        node_incremental = "<C-n>",
+        node_decremental = "<C-r>",
+        scope_incremental = "<C-s>",
+      },
+    },
     ensure_installed = {
       "typescript",
       "c",
@@ -16,9 +32,5 @@ return {
       "javascript",
       "html",
     },
-    sync_install = false,
-    auto_install = true,
-    highlight = { enable = true, additional_vim_regex_highlighting = false },
-    indent = { enable = true },
   },
 }
