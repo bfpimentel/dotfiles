7366e9c3b17d7aaed0f39a07f9bf64d6e4acd646
Bruno Pimentel <hello@bruno.so>
2024-12-30 17:24:45 -0300

    chore: add solaire client to wireguard

--- hosts/miquella/services/wireguard/default.nix
+++ hosts/miquella/services/wireguard/default.nix
@@ -1,4 +1,9 @@
-{ vars, config, pkgs, ... }:
+{
+  vars,
+  config,
+  pkgs,
+  ...
+}:
 
 {
   networking.firewall.allowedUDPPorts = [ 51820 ];
@@ -10,17 +15,32 @@
         ips = [ "10.22.10.1/24" ];
         listenPort = 51820;
         privateKeyFile = config.age.secrets.wireguard-miquella.path;
+        # postSetup = ''
+        #   ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.22.10.0/24 -o ${vars.networkInterface} -j MASQUERADE
+        # '';
         postSetup = ''
-          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.22.10.0/24 -o ${vars.networkInterface} -j MASQUERADE
+          ${pkgs.iptables}/bin/iptables -A FORWARD -i ${vars.wireguardInterface} -j ACCEPT;
+          ${pkgs.iptables}/bin/iptables -A FORWARD -o ${vars.wireguardInterface} -j ACCEPT;
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -o ${vars.networkInterface} -j MASQUERADE;
         '';
+        # postShutdown = ''
+        #   ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.22.10.0/24 -o ${vars.networkInterface} -j MASQUERADE
+        # '';
         postShutdown = ''
-          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.22.10.0/24 -o ${vars.networkInterface} -j MASQUERADE
+          ${pkgs.iptables}/bin/iptables -D FORWARD -i ${vars.wireguardInterface} -j ACCEPT;
+          ${pkgs.iptables}/bin/iptables -D FORWARD -o ${vars.wireguardInterface} -j ACCEPT;
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -o ${vars.networkInterface} -j MASQUERADE;
         '';
         peers = [
           {
             name = "malenia";
             publicKey = "DGIv16Ow92a2EzupVjD5K8wm9F0dicocvIuhKO9YbXQ=";
-            allowedIPs = [ "10.22.10.2/32" ];
+            allowedIPs = [ "10.22.10.2/32" "10.22.4.0/24" ];
+          }
+          {
+            name = "solaire";
+            publicKey = "xMWICTNi398NCBj8DS3085R4jbqXZBSECyq3pWmx+U4=";
+            allowedIPs = [ "10.22.10.3/32" ];
           }
         ];
       };
