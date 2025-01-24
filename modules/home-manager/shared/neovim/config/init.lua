5b323cc39eda275630393b22c4c2ef5223ddb010
Bruno Pimentel <hello@bruno.so>
2025-01-23 23:19:40 -0300

    fix: neovim clipboard

--- modules/home-manager/shared/neovim/config/init.lua
+++ modules/home-manager/shared/neovim/config/init.lua
@@ -25,22 +25,6 @@ vim.opt.scrolloff = 10
 
 require("configs.mappings")
 
--- Clipboard
-vim.opt.clipboard = "unnamedplus"
-
-local osc52 = require("vim.ui.clipboard.osc52")
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = osc52.copy("+"),
-    ["*"] = osc52.copy("*"),
-  },
-  paste = {
-    ["+"] = osc52.paste("+"),
-    ["*"] = osc52.paste("*"),
-  },
-}
-
 -- Highlight Yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
--- modules/home-manager/shared/neovim/config/lua/configs/mappings.lua
+++ modules/home-manager/shared/neovim/config/lua/configs/mappings.lua
@@ -1,8 +1,26 @@
-local opts = { noremap = true, silent = true }
 local map = vim.keymap.set
 
+local function with_default_opts(extra_opts)
+  local opts = { noremap = true, silent = true }
+
+  for i = 1, #extra_opts do
+    opts[#opts + 1] = extra_opts[i]
+  end
+  return opts
+end
+
 -- General
-map("n", "<S-u>", "<C-r><CR>", opts)
+map("n", "<S-u>", "<C-r><CR>", with_default_opts({}))
+
+-- Clipboard
+map("n", "p", '"+p')
+map("n", "P", '"+P')
+map("n", "dd", '"+dd')
+map({ "n", "v" }, "d", '"+d')
+map({ "n", "v" }, "y", '"+y')
+map({ "n", "v" }, "Y", '"+yy')
+map({ "n", "v" }, "x", '"+x')
+map({ "n", "v" }, "X", '"+X')
 
 -- Formatting
 map("n", "<leader>ff", function()
@@ -10,13 +28,13 @@ map("n", "<leader>ff", function()
 end, { desc = "general Format File" })
 
 -- Window
-map("n", "<C-h>", "<CMD>wincmd h<CR>", opts)
-map("n", "<C-k>", "<CMD>wincmd k<CR>", opts)
-map("n", "<C-l>", "<CMD>wincmd l<CR>", opts)
-map("n", "<C-j>", "<CMD>wincmd j<CR>", opts)
+map("n", "<C-h>", "<CMD>wincmd h<CR>", with_default_opts({}))
+map("n", "<C-k>", "<CMD>wincmd k<CR>", with_default_opts({}))
+map("n", "<C-l>", "<CMD>wincmd l<CR>", with_default_opts({}))
+map("n", "<C-j>", "<CMD>wincmd j<CR>", with_default_opts({}))
 
 -- Search
-map("n", "<Esc>", "<CMD>nohlsearch<CR><CR>", opts)
+map("n", "<Esc>", "<CMD>nohlsearch<CR><CR>", with_default_opts({}))
 
 -- Terminal
 map({ "n" }, "<C-t>", function()
@@ -24,17 +42,22 @@ map({ "n" }, "<C-t>", function()
   local terminalInstance = Terminal:new({ hidden = true })
 
   terminalInstance:toggle()
-end, { desc = "terminal Toggle Terminal" }, opts)
+end, with_default_opts({ desc = "terminal Toggle Terminal" }))
 
 map({ "n" }, "<leader>gg", function()
   local Terminal = require('toggleterm.terminal').Terminal
   local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
 
   lazygit:toggle()
-end, { desc = "terminal Toggle LazyGit" }, opts)
+end, with_default_opts({ desc = "terminal Toggle LazyGit" }))
 
 -- Tree
-map("n", "<leader>ns", ":Neotree toggle=true source=filesystem action=focus<CR>", { desc = "tree Show Tree" }, opts)
+map(
+  "n",
+  "<leader>ns",
+  ":Neotree toggle=true source=filesystem action=focus<CR>",
+  with_default_opts({ desc = "tree Show Tree" })
+)
 
 -- map("n", "<leader>ft", function()
 --   require("nvchad.themes").open()
