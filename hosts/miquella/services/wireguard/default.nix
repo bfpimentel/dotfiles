1387c45455b9dd2be842c008a5c1d3f68719a582
Bruno Pimentel <hello@bruno.so>
2025-07-04 14:37:01 -0300

    fix: vpn keys

--- hosts/miquella/services/wireguard/default.nix
+++ hosts/miquella/services/wireguard/default.nix
@@ -11,17 +11,17 @@
     enable = true;
     interfaces = {
       "${vars.wireguardInterface}" = {
-        ips = [ "10.22.10.2/24" ];
+        ips = [ "10.22.10.4/24" ];
         listenPort = 51820;
         privateKeyFile = config.age.secrets.wireguard-miquella.path;
         peers = [
           {
-            name = "malenia";
-            publicKey = "DGIv16Ow92a2EzupVjD5K8wm9F0dicocvIuhKO9YbXQ=";
-            endpoint = "vpn.luana.casa:51820";
+            name = "wg-lb";
+            publicKey = "IY3am4h3W9erXrMnbYkTMjY68lxHMZUUL0Cfd2ucSHA=";
+            endpoint = "vpn.jalotopimentel.com:51820";
             persistentKeepalive = 25;
             allowedIPs = [
-              "10.22.10.2/32"
+              "10.22.10.4/32"
               "10.22.4.0/24"
             ];
           }
--- modules/nixos/services/default.nix
+++ modules/nixos/services/default.nix
@@ -2,6 +2,6 @@
 
 {
   imports = [
-    ./glances
+    ./ssh
   ];
 }
