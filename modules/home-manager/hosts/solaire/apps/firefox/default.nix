130f94ce27afeea8bac9a8e2f3cf61ab03f452a8
Bruno Pimentel <hello@bruno.so>
2025-09-17 08:03:36 -0300

    chore: update aerospace config

--- modules/home-manager/hosts/solaire/apps/firefox/default.nix
+++ modules/home-manager/hosts/solaire/apps/firefox/default.nix
@@ -6,8 +6,8 @@
   # home.file."Library/Application\ Support/Firefox/Profiles/bfmp".source =
   #   homeManagerConfig.linkHostApp config "firefox";
 
-  textfox = {
-    enable = true;
-    profile = "c5pUpU3a.Profile\ 1";
-  };
+  # textfox = {
+  #   enable = true;
+  #   profile = "c5pUpU3a.Profile\ 1";
+  # };
 }
