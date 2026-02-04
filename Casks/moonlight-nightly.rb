c8b7fee39174747392bd6bfd07add7425bfd90c5
Bruno Pimentel <hello@bruno.so>
2026-02-04 13:10:43 -0300

    fix: trying to fix moonlight cask

--- Casks/moonlight-nightly.rb
+++ Casks/moonlight-nightly.rb
@@ -13,18 +13,13 @@ cask "moonlight-nightly" do
 
   preflight do
     Dir.chdir(staged_path) do
-      # 1. Update submodules
       system_command "git",
                      args: ["submodule", "update", "--init", "--recursive"]
 
-      # 2. Build the application
-      # We use the generate-dmg.sh script as it handles the full build process including deployment
       system_command "./scripts/generate-dmg.sh",
                      args: ["Release"],
                      env:  { "PATH" => "#{HOMEBREW_PREFIX}/bin:#{ENV["PATH"]}" }
 
-      # 3. Ad-hoc sign the application to run on Apple Silicon
-      # The script skips signing if no identity is provided, so we do it manually here.
       system_command "/usr/bin/codesign",
                      args: ["--force", "--deep", "-s", "-", "build/build-Release/app/Moonlight.app"]
     end
