fe5783c947488a014fbcc078ed6f952888948c2b
Bruno Pimentel <hello@bruno.so>
2026-04-29 12:00:04 -0300

    chore(cherubim): update openclaw insecure package version

--- modules/topology/outputs.nix
+++ modules/topology/outputs.nix
@@ -23,7 +23,7 @@ in
                 config = {
                   allowUnfree = true;
                   permittedInsecurePackages = [
-                    "openclaw-2026.4.12"
+                    "openclaw-2026.4.21"
                   ];
                 };
               };
